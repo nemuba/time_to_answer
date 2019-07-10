208c849010fc6f3793b9e02b0fd016669a582ad8
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-07-10 17:29:13 -0300

    basic authenticate http

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+  # Autenticação básica http
+  http_basic_authenticate_with :user=>"alef", :password => "123"
+
+  # Método index do controler Welcome
   def index
   end
 end
