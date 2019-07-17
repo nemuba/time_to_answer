5c5915b6c03c635520a787b04bd1a7593e79981d
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-07-17 20:06:39 -0300

    ajustando layout (site)

--- app/controllers/welcome_controller.rb
+++
@@ -1,9 +0,0 @@
-class WelcomeController < ApplicationController
-  # Autenticação básica http
-  #http_basic_authenticate_with :user=>"alef", :password => "123"
-  #before_action :authenticate_admin!
-  #before_action :authenticate_profile!
-  # Método index do controler Welcome
-  def index
-  end
-end
