3e924cdedb585f81646410e136e4692a263f083c
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-07-21 17:16:49 -0300

    criando de tela de novo e ediçao para admins_backoffice

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,5 +1,55 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :set_admin, only: [:edit, :update]
+  before_action :verify_password, only: [:update]
+
   def index
     @admins = Admin.all
   end
+
+  def new
+    @admin = Admin.new
+  end
+  
+  def create
+    @admin = Admin.new(permit_params)
+    
+    if @admin.save      
+      redirect_to admins_backoffice_admins_path, notice: "Criado com sucesso !"
+    else      
+      render :new
+    end
+  end
+  
+  def edit ;end
+
+  def update         
+    if @admin.update(permit_params)        
+      redirect_to admins_backoffice_admins_path, notice: "Administrador atualizado com sucesso !"
+    else        
+      render :edit
+    end
+  end
+  
+  
+private
+ 
+  def verify_password
+    # Permitindo so o parametro :email se o campo senha e confirmação estiverem em branco !
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end     
+  end
+
+
+  def permit_params  
+    # Permitindo alguns parametros   
+    params.require('admin').permit(:id,:email, :password, :password_confirmation)
+  end
+
+  # Setando admin
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+  
+ 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,26 @@
 Rails.application.routes.draw do
+
+  # localhost:3000/admins_backoffice/welcome/index
+  # localhost:3000/admins_backoffice/admins  GET
+  # localhost:3000/admins_backoffice/admins  POST
+  # localhost:3000/admins_backoffice/admins/new GET
+  # localhost:3000/admins_backoffice/admins/:id/edit PUTCH
   namespace :admins_backoffice do
     get 'welcome/index' # Dashboard
-    resources :admins, only: [:index] # Administradores
+    resources :admins, except: [:delete] # Administradores
   end
+  
+  # localhost:3000/site/welcome/index
   namespace :site do
     get 'welcome/index'
   end
+
+  # localhost:3000/users_backoffice/welcome/index
   namespace :users_backoffice do
     get 'welcome/index'
   end
+
+  # localhost:3000/admins_backoffice/welcome/index
   namespace :admins_backoffice do
     get 'welcome/index'
   end
@@ -16,8 +28,10 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
   
+  #localhost:3000/inicio
   get 'inicio', to: 'site/welcome#index'
   
+  #localhost:3000/
   root to: 'site/welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
