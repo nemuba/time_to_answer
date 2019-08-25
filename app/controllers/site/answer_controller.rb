31667dc87b7363074ee413103d53d007e990497f
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-08-25 20:28:56 -0300

    refatorando UserStatistic

--- app/controllers/site/answer_controller.rb
+++ app/controllers/site/answer_controller.rb
@@ -3,21 +3,7 @@ class Site::AnswerController < SiteController
   # route: /site/answer => POST
   def question
     @answer = Answer.find(params[:answer_id])       
-    set_user_statistic(@answer)
-  end
-
-  private 
-
-  def set_user_statistic(answer)
-    if user_signed_in?
-      user_statistic = UserStatistic.find_or_create_by(user: current_user)
-      if answer.correct?
-        user_statistic.right_questions += 1
-      else
-        user_statistic.wrong_questions += 1
-      end
-      user_statistic.save
-    end
+    UserStatistic.set_statistic(@answer, current_user)
   end
   
 end
