a61ef0404ab5267eed2d78fc116ca76028806821
Alef Ojeda de Oliveira <nemubatubag@gmail.com>
2019-08-25 22:14:38 -0300

    Configurando MailTrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,18 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   # Devise config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Mailtrap config
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => 'c59f696150e8f4',
+    :password => '397c351af916ef',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
+  }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
